1976324201cafe6c5ee932f2d7cb2f9b628126f6
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-12 17:49:57 +0200

    site counter persistence

--- frontend/guestbook/server.js
+++ frontend/guestbook/server.js
@@ -3,12 +3,22 @@ var ecstatic = require('ecstatic')
 var st = ecstatic(__dirname + '/public')
 var onend = require('end-of-stream')
 
+var level = require('level')
+var db = level('./counter.db')
+
 var counter = 0
+db.get('counter', function (err, value) {
+  counter = Number(value || 0)
+})
+
 var server = http.createServer(function (req, res) {
   console.log(req.method, req.url, req.headers)
   res.setHeader('set-cookie','visited-before=')
   if (req.url === '/' && !req.headers.cookie) {
     counter++
+    db.put('counter', counter, function (err) {
+      if (err) console.error(err)
+    })
     streams.forEach(function (stream) {
       stream.write(counter + '\n')
     })
