d039178def66ed5540c47b212b6010dc4238b241
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-12 17:55:27 +0200

    browser counter

--- db/count.js
+++ db/count.js
@@ -1,10 +1,20 @@
-var level = require('level')
+var level = require('level-browserify')
 var db = level('./cool.db')
+var html = require('yo-yo')
+var root = document.body.appendChild(document.createElement('div'))
+
+var count = '?'
+update()
+function update () {
+  html.update(root, html`<div>
+    <h1>COUNT=${count}</h1>
+  </div>`)
+}
 
 db.get('count', function (err, value) {
-  var count = Number(value || 0) + 1
+  count = Number(value || 0) + 1
+  update()
   db.put('count', count, function (err) {
     if (err) console.error(err)
-    else console.log(count)
   })
 })
