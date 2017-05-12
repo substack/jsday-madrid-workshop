6c87814a150128e264666eafc7f4826156fc6694
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-12 17:03:16 +0200

    cookie tracker

--- frontend/guestbook/server.js
+++ frontend/guestbook/server.js
@@ -5,8 +5,9 @@ var onend = require('end-of-stream')
 
 var counter = 0
 var server = http.createServer(function (req, res) {
-  console.log(req.method, req.url)
-  if (req.url === '/') {
+  console.log(req.method, req.url, req.headers)
+  res.setHeader('set-cookie','visited-before=')
+  if (req.url === '/' && !req.headers.cookie) {
     counter++
     streams.forEach(function (stream) {
       stream.write(counter + '\n')
